8ed16bf08a0985c9e58234133cdc9a40f437509e
Florian Schulze <mail@florian-schulze.net>
2019-06-30 12:01:31 +0200

    Prepare 0.2.0 release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='lektor-rst',
     description='Adds reStructuredText support to Lektor.',
-    version='0.1',
+    version='0.2.0',
     author=u'Florian Schulze',
     author_email='florian.schulze@gmx.net',
     url='http://github.com/fschulze/lektor-rst',
